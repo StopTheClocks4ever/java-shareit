4e3a2fe362b85312f003c572e67483a351339e07
StopTheClocks <stoptheclocksforever@yandex.ru>
2024-04-01 22:56:10 +0300

    Code-style correction-2

--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.UserMapper;
 import ru.practicum.shareit.user.UserValidator;
